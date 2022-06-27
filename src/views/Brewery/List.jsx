74a457e66bf02e647f87fd748a3f506b6fe77f2a
Zach Neibaur <zneibaur@protonmail.com>
2022-06-27 10:11:08 -0600

    Adding some basic list sorting
    List is set to sort ascending by default. The list can also be set to filter descending and then be toggled back and forth each time afterwards.

--- src/views/Brewery/List.jsx
+++ src/views/Brewery/List.jsx
@@ -3,18 +3,20 @@ import { Link } from 'react-router-dom';
 
 export default function BreweryList() {
   const [breweries, setBreweries] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('descending');
+  const [searchTerm, setSearchTerm] = useState('');
+  const [sortingToggle, setSortingToggle] = useState('Ascending');
 
   const getBreweryList = useCallback(
     async () => {
       try {
         const res = await fetch(`https://api.openbrewerydb.org/breweries?per_page=10`);
         const data = await res.json();
-        setBreweries(data);
+        const sortedData = sortingToggle === 'Ascending' ? data.sort((a, b) => a.name.localeCompare(b.name)) : data.sort((a, b) => b.name.localeCompare(a.name));
+        setBreweries(sortedData);
       } catch (error) {
         console.error(error);
       }
-    }, []
+    }, [sortingToggle]
   )
 
   useEffect(() => {
@@ -45,10 +47,10 @@ export default function BreweryList() {
         <button type='submit'>Search</button>
         <button type='reset' onClick={handleReset}>Reset</button>
       </form>
+      <button onClick={() => setSortingToggle(sortingToggle === 'Ascending' ? 'Descending' : 'Ascending')}>
+        Sort {sortingToggle === 'Ascending' ? 'Descending' : 'Ascending'}
+      </button>
       <ul>
-        <li>
-          <Link to='/breweries/1'>Brewery 1</Link> - Brewtown, OR
-        </li>
         {breweries?.length > 0 && breweries.map(({ city, id, name, state}) => (
           <li key={id}>
             <Link to={`/breweries/${id}`}>{name}</Link> - {city}, {state}
