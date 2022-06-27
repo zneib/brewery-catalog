bb7531f92cf9e46b423eba2e45d08eb25f2110c9
Zach Neibaur <zneibaur@protonmail.com>
2022-06-27 10:21:29 -0600

    Adding the Details view
    Adding some basic logic to view the listed breweries in greater detail.

--- src/views/Brewery/Detail.jsx
+++ src/views/Brewery/Detail.jsx
@@ -1,18 +1,37 @@
+import { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 
 export default function BreweryDetail() {
+  const [brewery, setBrewery] = useState();
   const { id } = useParams();
+  
+  useEffect(() => {
+    const getSingleBrewery = async () => {
+      try {
+        const res = await fetch(`https://api.openbrewerydb.org/breweries/${id}`);
+        const data = await res.json();
+        setBrewery(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getSingleBrewery();
+  }, [id])
 
-  return (
-    <main>
-      <h1>Brewery {id}</h1>
-      <p>Brewtown, Oregon 12345</p>
-      <p>United States</p>
-      <p>8005551234</p>
-      <p>
-        <a href='https://example.com'>View Website</a>
-      </p>
-      <Link to='/breweries'>Back to Breweries</Link>
-    </main>
-  );
+  if (!brewery) {
+    return <p>Loading...</p>
+  } else {
+    return (
+      <main>
+        <h1>Brewery {id}</h1>
+        <p>{brewery?.city}, {brewery?.state} {brewery?.postal_code}</p>
+        <p>{brewery?.country}</p>
+        <p>{brewery?.phone ? brewery?.phone : 'N/A' }</p>
+        <p>
+          <a href={brewery?.website_url}>View Website</a>
+        </p>
+        <Link to='/breweries'>Back to Breweries</Link>
+      </main>
+    )
+  }
 }
